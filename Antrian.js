a8738d54c5ef00980bdf5d1a4cab656fa0afb440
unknown <zidaneizd@gmail.com>
2025-07-02 21:50:05 +0700

    antrian ga nambah bang

--- Antrian.js
+++ Antrian.js
@@ -1,3 +1,36 @@
 let antrian = []
 
-function a ()
+function ambilAntrian() {
+    const nama = document.getElementById("input-name").value.trim()
+
+    if(!nama) {
+        alert("isi nama lu bro")
+    }
+
+    antrian.push(nama)
+    alert(`${nama} masuk keantrian`)
+} 
+
+function melayaniAntrian() {
+    if(antrian.length===0) {
+        alert("kosong masbro")
+
+    return
+    }
+    const namaYangDilayani = antrian.shift()
+}
+
+function totalAntrian() {
+    const totalJumlahAntrian = document.getElementById("jumlah-antrian")
+    const daftarAntrian = document.getElementById("daftar-antrian")
+    const listAntrian = ""
+
+    for (let i = 0; i < antrian.length; i++) {
+        listAntrian += `<li>${i+1}. ${antrian[i]}</li>`
+    }
+
+    daftarAntrian.innerHTML = listAntrian
+}
+
+
+
